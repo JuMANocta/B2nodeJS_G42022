6f9374302d62911e539c886221ce0eb38291b0f5
juman <ju@ju.com>
2022-02-16 10:22:06 +0100

    utilisation de son propre module 'hello'

---
+++ module/app.js
@@ -0,0 +1,3 @@
+const maVariable = require('./hello')
+// l'utilisation de './' permet d'importer des fichiers relatifs au fichier courant
+console.log(maVariable)
---
+++ module/hello.js
@@ -0,0 +1,5 @@
+let nom = 'Julien'
+let hello = 'Bonjour'
+
+// seul la variable 'hello' est exportée
+module.exports = hello
